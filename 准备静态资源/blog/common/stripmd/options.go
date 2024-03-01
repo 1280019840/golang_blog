fff0cbae74ee7356a4643c71fc785784f83f4911
zjh <136102971+1280019840@users.noreply.github.com>
2024-03-01 22:07:44 +0800

    Delete 准备静态资源/blog directory

--- 准备静态资源/blog/common/stripmd/options.go
+++
@@ -1,5 +0,0 @@
-package stripmd
-
-type Options struct {
-	SkipImages bool
-}
--- 准备静态资源/blog/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "blog/common/initialize"
-
-func main() {
-	initialize.LoadConfig()
-	initialize.Mysql()
-	initialize.Router()
-}
